7dc1c5aa85ac95c471d5654668e18d61f9bd5ebb
Kurt Schoenhoff <kurtjcu@users.noreply.github.com>
2017-08-10 05:57:53 +1000

    Added more code comments

--- ZumoBTRCV003/ZumoBTRCV003.ino
+++ ZumoBTRCV003/ZumoBTRCV003.ino
@@ -1,10 +1,21 @@
+/* 2017-08-10
+ * This file was created by Kurt@nqmakersupplies.com.au
+ * Converting the throttle and steering into motor speeds is mostly code from
+ * Pololu RC Zumo https://www.pololu.com/docs/0J57/7.a this code comes in the examples provided with the zumo libraries.
+ * Note: on my version of arduino some of the Plololu Examples are broken and I had to find the example via the desktop.
+ * Desigend to use with https://github.com/kurtjcu/JoystickControlToBT
+ */
+
+/**** if you are new or having some problems see the bottom of the file for sopme debugging hints ***/
+ 
+
 #include <ZumoMotors.h>
 
 #define LED_PIN       13 // user LED pin
 
 #define MAX_SPEED          400 // max motor speed
 #define JOYSTICK_DEADBAND   10 // compensate for control centering offset
-#define JOYSTICK_RANGE     512 // pulse width difference from 512 us to be treated as full scale input (for example, a value of 512 means
+#define JOYSTICK_RANGE     512 // difference from 512 (center position of joystick to be treated as full scale input (for example, a value of 512 means
                                //   any value <= 0 or >= 1023 is considered full scale)
 String inputString = "";
 char str[255];
@@ -16,10 +27,13 @@ int left_speed, right_speed;
 
 void setup() {
 
+  // Setup throttle and steering to be at the modpoint.
+  // this prevents the zumo running away before a control link is established.
   throttle = 512;
   newThrottle = throttle;
   steering = 512;
   newSteering = steering;
+  
   pinMode(LED_PIN, OUTPUT);
   digitalWrite(LED_PIN, LOW);
   Serial.begin(9600); // Default communication rate of the Bluetooth module
@@ -38,11 +52,13 @@ void loop() {
     }
   } else {
     
-    // RC signals encode information in pulse width centered on 512 (microseconds); subtract 512 to get a value centered on 0
+    // RC signals encode information centered on 512 (analog in midpoint 1 - 1023); subtract 512 to get a value centered on 0
+    // this conversion get us ready to use +- values for ZumoMotors
     int throttleForCalc = throttle - 512;
     int steeringForCalc = steering - 512;
 
-    // apply deadband
+    // apply deadband, deadband is giving us a little play with the center (home) position of the joystick.
+    // cheap joysticks rarely sit EXACTLY on the midpoint, this code stops the robot crawling away when joystick is at rest.
     if (abs(throttleForCalc) <= JOYSTICK_DEADBAND)
       throttleForCalc = 0;
     if (abs(steeringForCalc) <= JOYSTICK_DEADBAND)
@@ -57,24 +73,22 @@ void loop() {
     right_speed = min(max(right_speed, -MAX_SPEED), MAX_SPEED);
     
   }
-
+  
+  // Send the motor speeds to the motors.
+  ZumoMotors::setSpeeds(left_speed, right_speed);
+  
+  /* Debugging to serial port, uncomment if testing communications link from joystick without connecting a zumo.
   Serial.print("left: ");
   Serial.println(left_speed);
   Serial.print("Right: ");
   Serial.println(right_speed);
-
-  ZumoMotors::setSpeeds(left_speed, right_speed);
+  */
   
- /*
- if (state == '0') {
-  digitalWrite(ledPin, LOW); // Turn LED OFF
-  Serial.println("LED: OFF"); // Send back, to the phone, the String "LED: ON"
-  state = 0;
- }
- else if (state == '1') {
-  digitalWrite(ledPin, HIGH);
-  Serial.println("LED: ON");;
-  state = 0;
- } 
- */
+  /* This file was designed to use the HC-05 bluetooth module https://nqmakersupplies.com.au/shop/product/hc-05-bluetooth-rx-or-tx-142 
+   * The BT modules have to be paired VIA AT commands to work in this sketch. This sketch will also function using zigbee or any other transparent wireless link.
+   * For testing you can try connecting the RX/TX from each arduino without your radio links, this can help you rile out any radio configuration issues.
+   */
+
+  
+  
 }
